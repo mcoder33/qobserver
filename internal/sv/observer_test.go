72a15532f8a58cea0c23dfa198c63c61668cd3b6
coder33 <coder33@yandex.ru>
2025-08-10 23:46:58 +0400

    вынес в пакет sv и адаптироввал тест

---
+++ internal/sv/observer_test.go
@@ -0,0 +1,75 @@
+package sv
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+type observeTestSet struct {
+	qName string
+	out   string
+	qInfo *QueueInfo
+}
+
+func TestObserve(t *testing.T) {
+	testSet := map[string]observeTestSet{
+		"testZero": {
+			out: `
+Jobs
+- waiting: 0
+- delayed: 0
+- reserved: 0
+- done: 0
+`,
+			qInfo: &QueueInfo{
+				Name:     "testZero",
+				Waiting:  0,
+				Delayed:  0,
+				Reserved: 0,
+				Done:     0,
+			},
+		},
+		"testFilled": {
+			out: `
+Jobs
+- waiting: 11
+- delayed: 22
+- reserved: 33
+- done: 44
+`,
+			qInfo: &QueueInfo{
+				Name:     "testFilled",
+				Waiting:  11,
+				Delayed:  22,
+				Reserved: 33,
+				Done:     44,
+			},
+		},
+	}
+
+	var commands []*Cmd
+	for _, test := range testSet {
+		commands = append(commands, NewCmd(test.qInfo.Name, []string{"any", "cmd"}, func(ctx context.Context, name string, arg ...string) ([]byte, error) {
+			return []byte(test.out), nil
+		}))
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	res := map[string]*QueueInfo{}
+	obs := NewObserver(1*time.Second, 1*time.Second)
+	for qi := range obs.Run(ctx, commands) {
+		res[qi.Name] = qi
+		if len(res) == len(commands) {
+			cancel()
+		}
+	}
+
+	require.Equal(t, len(commands), len(res))
+	for qName, data := range testSet {
+		require.Equal(t, data.qInfo, res[qName])
+	}
+}
