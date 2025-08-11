b93dcb54e8b3b0cb58bb8d916c3b3a99537c27f9
coder33 <coder33@yandex.ru>
2025-08-11 08:17:20 +0400

    добавлены флаги для ТГ и границы уведомления

--- cmd/qobserver.go
+++ cmd/qobserver.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"os/signal"
 	"qobserver/internal/svr"
@@ -13,13 +15,21 @@ import (
 
 var (
 	configDir = flag.String("config", "/etc/supervisor/conf.d", "Path to supervisor conf.d directory")
+	tgToken   = flag.String("tg-token", "", "Telegram bot token")
+	tgChatID  = flag.String("tg-chat-id", "", "Telegram chat ID")
 	sleep     = flag.Duration("sleep", 1*time.Second, "Sleep between info executing in seconds; use 1s,2s,Ns...")
 	ttl       = flag.Duration("ttl", 5*time.Second, "Command execution ttl; use 1s,2s,Ns...")
-	//threshold = flag.Int("threshold", 1000, "Threshold for waiting alert")
+	threshold = flag.Int("threshold", 1000, "Threshold for waiting alert")
 )
 
 func main() {
 	flag.Parse()
+
+	if *tgToken == "" || *tgChatID == "" {
+		fmt.Println("tg-token and tg-chat-id are required")
+		flag.Usage()
+		os.Exit(1)
+	}
 
 	cmdPool := svr.NewCmdPool(func(ctx context.Context, name string, arg ...string) ([]byte, error) {
 		return exec.Command(name, arg...).Output()
